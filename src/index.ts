7480e78a57e77383430c1c19f1907b304c9326a6
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-01-15 11:17:02 -0300

    Fetch and augment data with ids

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+import * as d3 from "d3";
+
+const url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json";
+
+d3.json(url, function(error, graph) {
+  if (error) throw error;
+
+  graph.nodes.forEach((node, i) => node.id = i);
+
+  console.log(graph);
+});
